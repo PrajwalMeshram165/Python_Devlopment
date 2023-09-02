b5c22c9d0bb094ec10b0e89fe4c18c3c45a1bacd
PrajwalMeshram165 <prajwalmeshram165@gmail.com>
2023-09-02 10:02:22 +0530

    FIRST COMMIT

---
+++ PalindromeCheker.py
@@ -0,0 +1,6 @@
+string = input("Enter the string :")
+reverString = string[::-1]
+if(string == reverString):
+    print("the string is palindrome")
+else:
+    print("The string in not pallindrome")
